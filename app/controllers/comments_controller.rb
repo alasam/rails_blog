2e464c2d88ac146ccbf9b60ad7ac47416d0f0def
Anthony <alasam@live.com>
2022-01-06 08:09:25 -0500

    comment functionality working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
+  # create comment
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
 
+  # only accept certain parameters
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
