62a429478ae8f5833d2ee9c43b9f0170414d4640
Anthony <alasam@live.com>
2022-01-06 07:42:44 -0500

    Add edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,25 @@
 class ArticlesController < ApplicationController
+  # index page
   def index
     @articles = Article.all
   end
 
+  # show page (list of forms created)
   def show
     @article = Article.find(params[:id])
   end
 
+  # new page (shows title and post)
   def new
     @article = Article.new
   end
 
+  # edit
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  # creating/saving new post
   def create
     @article = Article.new(article_params)
    
@@ -21,6 +30,18 @@ class ArticlesController < ApplicationController
     end
   end
    
+  # submitting update
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  #only accepting certain entries
   private
     def article_params
       params.require(:article).permit(:title, :text)
